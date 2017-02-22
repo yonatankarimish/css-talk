3de39a4eb342e384b6f9ee05b5c851159c3fdf04
Yonatan Alon <yonatankarimish@gmail.com>
2017-02-23 01:18:18 +0200

    finished basic layout and ui router config

--- sources/app.js
+++ sources/app.js
@@ -1,1 +1,10 @@
 angular.module('app', ['ngAnimate', 'ngTouch', 'ui.router', 'ui.bootstrap']);
+
+angular.module('app').config(function($stateProvider, $urlRouterProvider) {
+     $urlRouterProvider.otherwise('/welcome');
+     $stateProvider
+         .state('welcome', {
+             url: '/welcome',
+             templateUrl: 'partials/welcome.html'
+         });
+ });
