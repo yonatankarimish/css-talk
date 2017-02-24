f36370d93035451f6ff2b29cbfc0c1a689b7b66b
Yonatan Alon <yonatankarimish@gmail.com>
2017-02-24 15:08:45 +0200

    navigation between partials. headlines created

--- sources/app.js
+++ sources/app.js
@@ -3,8 +3,52 @@ angular.module('app', ['ngAnimate', 'ngTouch', 'ui.router', 'ui.bootstrap']);
 angular.module('app').config(function($stateProvider, $urlRouterProvider) {
      $urlRouterProvider.otherwise('/welcome');
      $stateProvider
-         .state('welcome', {
-             url: '/welcome',
-             templateUrl: 'partials/welcome.html'
-         });
- });
+        .state('welcome', {
+            url: '/welcome',
+            templateUrl: 'partials/welcome.html'
+        })
+        .state('animations', {
+            url: '/animations',
+            templateUrl: 'partials/animations.html'
+        })
+        .state('boxModel', {
+            url: '/box-model',
+            templateUrl: 'partials/box-model.html'
+        })
+        .state('crossBrowser', {
+            url: '/cross-browser',
+            templateUrl: 'partials/cross-browser.html'
+        })
+        .state('flex', {
+            url: '/flex',
+            templateUrl: 'partials/flex.html'
+        })
+        .state('goodPractices', {
+            url: '/good-practices',
+            templateUrl: 'partials/good-practices.html'
+        })
+        .state('measure', {
+            url: '/measure',
+            templateUrl: 'partials/measure.html'
+        })
+        .state('multiClasses', {
+            url: '/multi-classes',
+            templateUrl: 'partials/multi-classes.html'
+        })
+        .state('ngClass', {
+            url: '/ng-class',
+            templateUrl: 'partials/ng-class.html'
+        })
+        .state('responsive', {
+            url: '/responsive',
+            templateUrl: 'partials/responsive.html'
+        })
+        .state('selectors', {
+            url: '/selectors',
+            templateUrl: 'partials/selectors.html'
+        })
+        .state('specificity', {
+            url: '/specificity',
+            templateUrl: 'partials/specificity.html'
+        });
+});
---
+++ sources/scrollController.js
@@ -0,0 +1,17 @@
+angular.module('app').controller('scrollController', ['$state',
+    function($state){
+        var ctrl = this;
+
+        ctrl.views = ['welcome', 'boxModel', 'measure', 'selectors', 'multiClasses', 'goodPractices', 'specificity', 'responsive', 'ngClass', 'flex', 'animations', 'goodPractices'];
+        ctrl.currentView = 0;
+
+        ctrl.next = function(){
+            ctrl.currentView = (ctrl.currentView == ctrl.views.length-1? 0: ctrl.currentView+1);
+            $state.go(ctrl.views[ctrl.currentView]);
+        };
+
+        ctrl.previous = function(){
+            ctrl.currentView = (ctrl.currentView > 0? ctrl.currentView-1 : ctrl.views.length-1);
+            $state.go(ctrl.views[ctrl.currentView]);
+        };
+}]);
